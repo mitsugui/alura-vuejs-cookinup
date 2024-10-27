d30fa94005d67ff401867a3f734972fa6c4a68ad
mitsugui <julio.kawai@gmail.com>
2024-10-27 10:00:52 -0300

    Commit inicial final aula 1 entendendo componentes

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+
+export default {
+  components: { Banner : Banner }
+}
+
+</script>
+
+<template>
+  <Banner />
+
+  <h1 class="titulo">
+    Texto do App.vue
+  </h1>
+</template>
+
+<style scoped>
+.titulo {
+  color: red;
+}
+</style>
