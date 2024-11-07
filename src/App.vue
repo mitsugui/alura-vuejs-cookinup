0c88fa36ecb1086bd047c345b7848670d023092a
mitsugui <julio.kawai@gmail.com>
2024-11-06 23:47:29 -0300

    Impressão de uma lista de ingredientes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
 
 export default {
-  components: { Banner : Banner }
+  components: {
+    Banner: Banner,
+    ConteudoPrincipal: ConteudoPrincipal
+  }
 }
-
 </script>
 
 <template>
   <Banner />
-
-  <h1 class="titulo">
-    Texto do App.vue
-  </h1>
+  <ConteudoPrincipal />
 </template>
 
-<style scoped>
-.titulo {
-  color: red;
-}
-</style>
+<style scoped></style>
