46258d4e383e380f987b12acc7bdd1274a4ad7e9
mitsugui <julio.kawai@gmail.com>
2024-11-08 00:38:53 -0300

    Componentização da SuaLista

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
-import Tag from './Tag.vue';
+import SuaLista from './SuaLista.vue';
 
 export default {
     data() {
@@ -9,30 +9,15 @@ export default {
         }
     },
     components: {
-        SelecionarIngredientes,
-        Tag
+        SuaLista,
+        SelecionarIngredientes
     }
 }
 </script>
 
 <template>
     <main class="conteudo-principal">
-        <section>
-            <span class="subtitulo-lg sua-lista-texto">
-                Sua lista:
-            </span>
-
-            <ul v-if="ingredientes.length > 0" class="ingredientes-sua-lista">
-                <li v-for="ingrediente in ingredientes" :key="ingrediente">
-                    <Tag :texto="ingrediente" />
-                </li>
-            </ul>
-
-            <p v-else class="paragrafo lista-vazia">
-                <img src="../assets/images/icones/lista-vazia.svg" alt="Ícone de pesquisa">
-                Sua lista de ingredientes está vazia. Adicione ingredientes para começar a explorar novas receitas!
-            </p>
-        </section>
+        <SuaLista :ingredientes="ingredientes" />
 
         <SelecionarIngredientes />
     </main>
@@ -49,31 +34,6 @@ export default {
     flex-direction: column;
     align-items: center;
     gap: 5rem;
-}
-
-.sua-lista-texto {
-    color: var(--coral, #F0633C);
-    display: block;
-    text-align: center;
-    margin-bottom: 1.5rem;
-}
-
-.ingredientes-sua-lista {
-    display: flex;
-    justify-content: center;
-    gap: 1rem 1.5rem;
-    flex-wrap: wrap;
-}
-
-.lista-vazia {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 0.25rem;
-
-    color: var(--coral, #F0633C);
-    text-align: center;
 }
 
 @media only screen and (max-width: 1300px) {
