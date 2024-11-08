a09e7b262cd892e960c9de69b61d1cfa395e847b
mitsugui <julio.kawai@gmail.com>
2024-11-08 00:09:31 -0300

    Adição de ingreditentes

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,37 +1,41 @@
 <script lang="ts">
 import { obterCategorias } from '@/http/index';
 import type ICategoria from '@/interfaces/ICategoria';
+import CardCategoria from './CardCategoria.vue';
 
 export default {
-    data() {
-        return {
-            categorias: [] as ICategoria[]
-        }
-    },
-    async created() {
-        this.categorias = await obterCategorias();
+  data() {
+    return {
+      categorias: [] as ICategoria[]
     }
+  },
+  async created() {
+    this.categorias = await obterCategorias();
+  },
+  components: {
+    CardCategoria
+  }
 }
 </script>
 
 <template>
-    <section class="selecionar-ingredientes">
-        <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
+  <section class="selecionar-ingredientes">
+    <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
 
-        <p class="paragrafo-lg instrucoes">
-            Selecionar abaixo os ingredientes que você quer usar nesta receita:
-        </p>
+    <p class="paragrafo-lg instrucoes">
+      Selecionar abaixo os ingredientes que você quer usar nesta receita:
+    </p>
 
-        <ul class="categorias">
-            <li v-for="categoria in categorias" :key="categoria.nome" class="categoria">
-                {{ categoria.nome }}
-            </li>
-        </ul>
+    <ul class="categorias">
+      <li v-for="categoria in categorias" :key="categoria.nome">
+        <CardCategoria :categoria="categoria" />
+      </li>
+    </ul>
 
-        <p class="paragrafo dica">
-            *Atenção: consideramos que você tem em casa sal, pimenta e água.
-        </p>
-    </section>
+    <p class="paragrafo dica">
+      *Atenção: consideramos que você tem em casa sal, pimenta e água.
+    </p>
+  </section>
 </template>
 
 <style scoped>
