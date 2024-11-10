5f0e34f02ea650c028640f857e5a039d18713aa0
mitsugui <julio.kawai@gmail.com>
2024-11-10 03:58:53 -0300

    Ajustes visuais lista receitas

--- src/components/CardReceita.vue
+++ src/components/CardReceita.vue
@@ -13,14 +13,43 @@ export default {
 </script>
 
 <template>
-    <article class="card-receita">
+    <article class="receita">
         <header>
-            <img :src="`/imagens/receitas/${receita.imagem}`" alt="">
+            <img :src="`/imagens/receitas/${receita.imagem}`" alt="" class="imagem">
 
-            <h2 class="paragrafo-lg receita__nome">{{ receita.nome }}</h2>
+            <h2 class="paragrafo receita__nome">{{ receita.nome }}</h2>
         </header>
     </article>
 </template>
 
 <style scoped>
+.receita {
+    width: 17.625rem;
+    border-radius: 1rem;
+    background: var(--branco, #FFF);
+    box-shadow: 4px 4px 10px 0px rgba(68, 68, 68, 0.05);
+    height: 14.75rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+}
+
+.imagem {
+    border-radius: 1rem 1rem 0 0;
+    display: block;
+    max-width: 17.625rem;
+    max-height: 9.375rem;
+    width: 17.625rem;
+    height: 9.375rem;
+}
+
+.receita__nome {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 5.375rem;
+    font-weight: 300;
+}
 </style>
--- src/components/MostrarReceitas.vue
+++ src/components/MostrarReceitas.vue
@@ -1,8 +1,8 @@
 <script lang="ts">
 import { obterReceitas } from '@/http/index';
 import type IReceita from '@/interfaces/IReceita';
-import BotaoPrincipal from './BotaoPrincipal.vue';
 import CardReceita from './CardReceita.vue';
+import BotaoPrincipal from './BotaoPrincipal.vue';
 
 export default {
     components: {
@@ -26,19 +26,22 @@ export default {
         <h1 class="cabecalho titulo-receitas">Receitas</h1>
 
         <p class="paragrafo-lg instrucoes">
-            Resultados encontrados: 8
+            Resultados encontrados: {{ receitas.length }}
         </p>
 
-        <p class="paragrafo-lg instrucoes">
+        <p v-if="receitas.length" class="paragrafo-lg instrucoes">
             Veja as opções de receitas que encontramos com os ingredientes que você tem por aí!
         </p>
+        <p v-else class="paragrafo-lg instrucoes">
+            Ops, não encontramos resultados para sua combinação. Vamos tentar de novo?
+        </p>
 
-
-        <ul class="receitas">
+        <ul v-if="receitas.length" class="receitas">
             <li v-for="receita in receitas" :key="receita.nome">
                 <CardReceita :receita="receita" />
             </li>
         </ul>
+        <img v-else src="../assets/images/sem-receitas.png" alt="Sem resultados" />
 
         <BotaoPrincipal :texto="'Editar lista'" @click="$emit('editarLista')" />
     </section>
@@ -55,5 +58,24 @@ export default {
     color: var(--verde-medio, #3D6D4A);
     display: block;
     margin-bottom: 1.5rem;
+}
+
+.instrucoes {
+    margin-bottom: 2rem;
+}
+
+.receitas {
+    margin-bottom: 1rem;
+    display: flex;
+    justify-content: center;
+    gap: 1.5rem;
+    flex-wrap: wrap;
+    list-style-type: none;
+}
+
+@media only screen and (max-width: 767px) {
+    .dica {
+        margin-bottom: 2.5rem;
+    }
 }
 </style>
